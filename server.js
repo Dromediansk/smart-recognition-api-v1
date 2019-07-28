dfc12b8fcee3691a4d76deb84d812052b448212c
Pillár <Miroslav.Pillar@senacor.com>
2019-07-28 10:41:05 +0200

    setting up db for local use

--- server.js
+++ server.js
@@ -10,6 +10,13 @@ const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
 const db = knex({
+  // client: 'pg',
+  // connection: {
+  //   host: '127.0.0.1',
+  //   user: 'postgres',
+  //   password: '',
+  //   database: 'postgres'
+  // }
   client: 'pg',
   connection: {
     connectionString: process.env.DATABASE_URL,
